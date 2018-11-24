9717f32073d7375c2fcb2b1ebef0a0b6406fd001
rnitta <attinyes@gmail.com>
2018-11-24 21:45:40 +0900

    Initial Commit

---
+++ GestureMD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GestureMD
+//
+//  Created by PT2051 on 2018/11/24.
+//  Copyright © 2018年 amagrammer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
